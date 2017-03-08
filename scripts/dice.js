d75b4e0f9fadcbd782fdca05b4c539fb93844070
jtpete <pastor.jpete@gmail.com>
2017-03-07 18:17:54 -0600

    added random first turn with arrow move.

--- scripts/dice.js
+++ scripts/dice.js
@@ -1,1 +1,16 @@
 "use strict";
+
+function firstTurn(){
+  return Math.round(Math.random());
+}
+
+function startArrowDirection(){
+  var direction = firstTurn();
+  if (direction === 1){
+    document.getElementById("arrowPic").src = "images/up.png"
+    console.log("arrow up" + direction)
+  }else {
+    document.getElementById("arrowPic").src = "images/down.png"
+    console.log("arrow up" + direction)
+  }
+}
