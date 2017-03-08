e738ec2bb83a5664c3a212eefc634362ff5d816d
jtpete <pastor.jpete@gmail.com>
2017-03-08 11:37:32 -0600

    Basic Game Complete

--- scripts/dice.js
+++ scripts/dice.js
@@ -7,10 +7,266 @@ function firstTurn(){
 function startArrowDirection(){
   var direction = firstTurn();
   if (direction === 1){
-    document.getElementById("arrowPic").src = "images/up.png"
-    console.log("arrow up" + direction)
+    document.getElementById("arrowPic").src = "images/up.png";
+    document.getElementById("turnID").innerHTML = "Hare Starts";
   }else {
-    document.getElementById("arrowPic").src = "images/down.png"
-    console.log("arrow up" + direction)
+    document.getElementById("arrowPic").src = "images/down.png";
+    document.getElementById("turnID").innerHTML = "Tortoise Starts";
   }
+}
+
+function startPlayerLocation(){
+  document.getElementById("hareImg").setAttribute("style", "margin-left: 0px");
+  document.getElementById("tortImg").setAttribute("style", "margin-left: 0px");
+}
+
+function startPlayerScores(){
+  document.getElementById("hareProgress").innerHTML = "0";
+  document.getElementById("tortProgress").innerHTML = "0";
+}
+
+function startGame(){
+  startArrowDirection();
+  startPlayerLocation();
+  startPlayerScores();
+  outputResult("Let's Begin", 1);
+}
+
+function varifyTurn(){
+    var turnId = document.getElementById("turnID").innerHTML;
+
+    return turnId.charAt(0)
+}
+
+function toggleTurn(currentTurn){
+    if(currentTurn === "T"){
+      document.getElementById("arrowPic").src = "images/up.png";
+      document.getElementById("turnID").innerHTML = "Hare's Turn";
+    }else{
+      document.getElementById("arrowPic").src = "images/down.png";
+      document.getElementById("turnID").innerHTML = "Tortoise's Turn";
+    }
+}
+
+function outputResult(result, level){
+  if (level != 1){
+    document.getElementById("results").setAttribute("class", "resultRoll");
+    document.getElementById("results").innerHTML = result;
+  }else {
+    document.getElementById("results").setAttribute("class", "resultError");
+    document.getElementById("results").innerHTML = result;
+
+  }
+}
+
+function rollDie(sides){
+  return ((Math.floor(Math.random() * sides)) + 1);
+}
+
+function movePlayer(player, length){
+
+  var newLength = 0;
+  if(player === "H"){
+    var currentLength = Number(document.getElementById("hareProgress").innerHTML);
+    if (currentLength + length < 0){
+      newLength = 0;
+    }else{
+      newLength = currentLength + length;
+    }
+    var pxLength = newLength * 10;
+    var moveLength = "margin-left: " + pxLength + "px";
+    document.getElementById("hareProgress").innerHTML = newLength;
+    document.getElementById("hareImg").setAttribute("style", moveLength);
+  }else{
+    var currentLength = Number(document.getElementById("tortProgress").innerHTML);
+    if (currentLength + length < 0){
+      newLength = 0;
+    }else{
+      newLength = currentLength + length;
+    }
+    var pxLength = newLength * 10;
+    var moveLength = "margin-left: " + pxLength + "px";
+    document.getElementById("tortProgress").innerHTML = newLength;
+    document.getElementById("tortImg").setAttribute("style", moveLength);
+  }
+}
+
+function rollEqualsWin(player, roll){
+  if(player === "H"){
+    var currentLength = Number(document.getElementById("hareProgress").innerHTML);
+    if (currentLength + roll >= 100){
+      return true;
+    }
+  }else{
+    var currentLength = Number(document.getElementById("tortProgress").innerHTML);
+    if (currentLength + roll >= 100){
+      return true;
+    }
+  }
+  return false;
+}
+
+function displayWinner(player){
+  if(player === "H"){
+    document.getElementById("hareProgress").innerHTML = "100";
+    document.getElementById("hareImg").setAttribute("style", "margin-left: 1000px");
+    outputResult("Hare Wins!", 1);
+  }else{
+    document.getElementById("tortProgress").innerHTML = "100";
+    document.getElementById("tortImg").setAttribute("style", "margin-left: 1000px");
+    outputResult("Tortoise Wins!", 1);
+    }
+}
+
+function applySpecialRolls(die, roll){
+  var returnRoll = 1;
+  if(die === 20){
+    if(roll >= 14 && roll <= 17){
+      returnRoll = roll*-1;
+    }else{
+      returnRoll = roll;
+    }
+  }else if(die === 12){
+    if(roll >= 6 && roll <= 9){
+      returnRoll = roll*-1;
+    }else{
+      returnRoll = roll;
+    }
+  }else if(die === 10){
+    if(roll >= 5 && roll <= 7){
+      returnRoll = roll*-1;
+    }else{
+      returnRoll = roll;
+    }
+  }else if(die === 8){
+    if(roll === 4 || roll === 5){
+      returnRoll = roll*-1;
+    }else{
+      returnRoll = roll;
+    }
+  }else if (die === 6){
+    if(roll === 3){
+      returnRoll = roll*-1;
+    }else{
+      returnRoll = roll;
+    }
+  }else if(die === 4){
+    returnRoll = roll;
+  }
+  return returnRoll;
+}
+
+
+function roll4(){
+    var turnId = varifyTurn();
+    var dieUsed = 4;
+    if (turnId != "C"){
+      var numberRoll = rollDie(dieUsed);
+      numberRoll = applySpecialRolls(dieUsed, numberRoll);
+      if (rollEqualsWin(turnId, numberRoll)){
+        displayWinner(turnId);
+      }else{
+        movePlayer(turnId, numberRoll);
+        outputResult(numberRoll);
+        toggleTurn(turnId);
+      }
+    }else{
+      var msg = "Please Click Play"
+      outputResult(msg, 1);
+    }
+}
+
+function roll6(){
+    var turnId = varifyTurn();
+    var dieUsed = 6;
+    if (turnId != "C"){
+      var numberRoll = rollDie(dieUsed);
+      numberRoll = applySpecialRolls(dieUsed, numberRoll);
+      if (rollEqualsWin(turnId, numberRoll)){
+        displayWinner(turnId);
+      }else{
+        movePlayer(turnId, numberRoll);
+        outputResult(numberRoll);
+        toggleTurn(turnId);
+      }
+    }else{
+      var msg = "Please Click Play"
+      outputResult(msg, 1);
+    }
+}
+
+function roll8(){
+    var turnId = varifyTurn();
+    var dieUsed = 8;
+    if (turnId != "C"){
+      var numberRoll = rollDie(dieUsed);
+      numberRoll = applySpecialRolls(dieUsed, numberRoll);
+      if (rollEqualsWin(turnId, numberRoll)){
+        displayWinner(turnId);
+      }else{
+        movePlayer(turnId, numberRoll);
+        outputResult(numberRoll);
+        toggleTurn(turnId);
+      }
+    }else{
+      var msg = "Please Click Play"
+      outputResult(msg, 1);
+    }
+}
+
+function roll10(){
+    var turnId = varifyTurn();
+    var dieUsed = 10;
+    if (turnId != "C"){
+      var numberRoll = rollDie(dieUsed);
+      numberRoll = applySpecialRolls(dieUsed, numberRoll);
+      if (rollEqualsWin(turnId, numberRoll)){
+        displayWinner(turnId);
+      }else{
+        movePlayer(turnId, numberRoll);
+        outputResult(numberRoll);
+        toggleTurn(turnId);
+      }
+    }else{
+      var msg = "Please Click Play"
+      outputResult(msg, 1);
+    }
+}
+
+function roll12(){
+    var turnId = varifyTurn();
+    var dieUsed = 12;
+    if (turnId != "C"){
+      var numberRoll = rollDie(dieUsed);
+      numberRoll = applySpecialRolls(dieUsed, numberRoll);
+      if (rollEqualsWin(turnId, numberRoll)){
+        displayWinner(turnId);
+      }else{
+        movePlayer(turnId, numberRoll);
+        outputResult(numberRoll);
+        toggleTurn(turnId);
+      }
+    }else{
+      var msg = "Please Click Play"
+      outputResult(msg, 1);
+    }
+}
+
+function roll20(){
+    var turnId = varifyTurn();
+    var dieUsed = 20;
+    if (turnId != "C"){
+      var numberRoll = rollDie(dieUsed);
+      numberRoll = applySpecialRolls(dieUsed, numberRoll);
+      if (rollEqualsWin(turnId, numberRoll)){
+        displayWinner(turnId);
+      }else{
+        movePlayer(turnId, numberRoll);
+        outputResult(numberRoll);
+        toggleTurn(turnId);
+      }
+    }else{
+      var msg = "Please Click Play"
+      outputResult(msg, 1);
+    }
 }
